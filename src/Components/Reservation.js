715baab5773aaddce8269f682dd8d105064370ef
Johanna Dekeyser <bleddyntreffot@gmail.com>
2023-05-23 18:24:46 +0200

    modification

--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -1,22 +1,21 @@
-import React,{useState,useRef} from 'react'
+import React,{useState } from 'react'
 
 export function Reservation () {
-    const [date, setDate] = useState('');
-    const dateInputRef = useRef(null);
+    const [date, setDate] = useState();
     
     const [person,setPerson] = useState('');
-    const [hour,SetHour] = useState('');
+
+    const [hour,setHour] = useState('');
+
     const handleChange = (e) => {
     setDate(e.target.value);
     };
     const handleChangePerson = (p)=>{
       setPerson(p.target.value);
     }
-
-    const handleClick = (h) => {
-      SetHour(h.target.value);
-    };
-
+    const handleChangeHour = (h)=>{
+      setHour(h.target.value);
+    }
   return (
     <>
     <h1>Réserver une table</h1>
@@ -26,36 +25,40 @@ export function Reservation () {
 <input
 type="date"
 onChange={handleChange}
-ref={dateInputRef}
 /></div>
       <div>
       <label >Nombre de couverts</label>
       <input type='number' onChange={handleChangePerson}></input>
       </div>
       <br></br>
-<h3>Déjeuner</h3>
-<button onClick={handleClick}></button>
-<button >11h15</button>
-<button >11h30</button>
-<button >11h45</button>
-<button >12h</button>
-<button >12h15</button>
-<button >12h30</button>
-<button >12h45</button>
-<button >13h</button>
-<br /><br />
-<h3>Diner</h3>
-<button >19h</button>
-<button >19h15</button>
-<button>19h30</button>
-<button>19h45</button>
-<button>20h</button>
-<button>20h15</button>
-<button>20h30</button>
-<button>20h45</button>
-<button>21h</button>
+      <label >Pour quelle heure?</label>
+<select onChange={handleChangeHour}>
+<option value="">--Déjeuner--</option>
+    		<option value="11h">11h</option>
+    		<option value="11h15">11h15</option>
+    		<option value="11h30">11h30</option>
+    		<option value="11h45">11h45</option>
+        <option value="12h">12h</option>
+    		<option value="12h15">12h15</option>
+    		<option value="12h30">12h30</option>
+    		<option value="12h45">12h45</option>
+        <option value="13h">13h</option>
+   		</select>
+       <select onChange={handleChangeHour}>
+<option value="">--Diner--</option>
+    		<option value="19h">19h</option>
+    		<option value="19h15">19h15</option>
+    		<option value="19h30">19h30</option>
+    		<option value="19h45">19h45</option>
+        <option value="20h">20h</option>
+    		<option value="20h15">20h15</option>
+    		<option value="20h30">20h30</option>
+    		<option value="20h45">20h45</option>
+        <option value="21h">21h</option>
+   		</select>
+
 <br></br>
-<label>vous avez choisi le {date} pour {person} personnes à {hour}</label>
+<label>vous avez choisi le {date} pour {person} personne(s) à {hour}</label>
 <br />
 <button type="submit">Réserver</button>
     </form>
